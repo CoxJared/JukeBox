ff886111a328ed62cca4969a3830e8ec246d1713
Jared Cox <coxj1@mcmaster.ca>
2020-08-08 22:30:21 -0400

    add nav bar and simple homepage grid

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+//MUI
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+const navbarStyle = {
+  container: {},
+  button: {
+    color: '#fff',
+    fontSize: 20
+  }
+};
 
 export class NavBar extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Button style={navbarStyle.button}>home</Button>
+        <Button style={navbarStyle.button}>explore</Button>
+        <Button style={navbarStyle.button}>lists</Button>
+        <Button style={navbarStyle.button}>activity</Button>
+      </Grid>
+    );
   }
 }
 
-export default NavBar
+export default NavBar;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import NavBar from '../components/layout/NavBar';
 
+import Grid from '@material-ui/core/Grid';
+import SearchBar from '../components/layout/SearchBar';
+import AlbumShowcase from '../components/albums/AlbumShowcase';
+
+const homeStyles = {
+  container: {
+    margin: ' 50px auto',
+    justifyContent: 'center'
+  }
+};
+
 export class home extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-      </div>
+      <Grid container spacing={2} style={homeStyles.container}>
+        <Grid item sm={3}>
+          <NavBar />
+        </Grid>
+        <Grid item sm={7}>
+          <SearchBar />
+          <AlbumShowcase />
+        </Grid>
+      </Grid>
     );
   }
 }
