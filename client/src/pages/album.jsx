85c8922551a0cd95ca20130a4839e05d336a4dff
Jared Cox <coxj1@mcmaster.ca>
2020-08-11 11:14:38 -0400

    added simple rating chart

--- client/src/pages/album.jsx
+++ client/src/pages/album.jsx
@@ -36,9 +36,18 @@ const styles = (theme) => ({
     albumText: {
         color: '#bbb'
     },
+    tracklistExpansion: {
+        marginTop: 10,
+        fontSize: 15,
+        fontWeight: 500,
+        textAlign: 'left',
+        float: 'left',
+        width: '100%',
+        cursor: 'pointer'
+    },
     tracks: {
         width: 900,
-        marginTop: 20
+        overflow: 'hidden'
     },
     track: {
         height: 20,
@@ -68,6 +77,49 @@ const styles = (theme) => ({
         width: 300,
         fontSize: 20,
         color: '#aaa'
+    },
+    ratingsChart: {
+        width: '100%',
+        position: 'relative',
+        backgroundColor: '#212122',
+        display: 'flex',
+        padding: 15,
+        marginTop: 20
+    },
+    ratingBar: {
+        width: 50,
+        backgroundColor: '#e9e950',
+        margin: '0 3px'
+    },
+    ratingBarTop: {
+        width: 50,
+        backgroundColor: '#212122'
+    },
+    ratingsTitle: {
+        color: '#e9e950',
+        fontSize: 15,
+        fontWeight: 400,
+        position: 'absolute',
+        top: 10,
+        left: 15
+    },
+    avgRatingContainer: {
+        width: 200,
+        height: 200,
+        margin: 20
+    },
+    avgRating: {
+        color: '#fff',
+        fontSize: 45,
+        fontWeight: 500,
+        width: '100%',
+        textAlign: 'center'
+    },
+    ratingsCount: {
+        color: '#aaa',
+        fontSize: 15,
+        width: '100%',
+        textAlign: 'center'
     }
 });
 
@@ -77,7 +129,7 @@ export class album extends Component {
     constructor(props) {
         super(props);
         const { album, artist } = props.location.state;
-        this.state = {};
+        this.state = { trackListOpened: false };
         this.getInfoFromApiRequest(album, artist);
     }
 
@@ -93,9 +145,71 @@ export class album extends Component {
         }
     }
 
+    handleTrackListExpansion = () => {
+        this.setState({ trackListOpened: !this.state.trackListOpened });
+    };
+
+    createBarChartElements = (ratings) => {
+        const { classes } = this.props;
+
+        const ratingCount = ratings.reduce(
+            (acc, ratings) => (acc += ratings),
+            0
+        );
+        const avgRating =
+            Math.round(
+                (ratings.reduce((acc, rating, i) => (acc += rating * i), 0) /
+                    ratingCount) *
+                    10
+            ) / 10;
+
+        let max = Math.max(...ratings);
+        let HEIGHT = 140;
+        console.log(max);
+        console.log((HEIGHT / max) * (max - ratings[0]));
+        console.log((HEIGHT / max) * (max - ratings[1]));
+        console.log((HEIGHT / max) * (max - ratings[2]));
+        console.log((HEIGHT / max) * (max - ratings[3]));
+        console.log((HEIGHT / max) * (max - ratings[4]));
+        console.log((HEIGHT / max) * (max - ratings[5]));
+
+        let elements = ratings.map((rating) => (
+            <div
+                className={this.props.classes.ratingBar}
+                style={{ height: HEIGHT }}
+            >
+                <div
+                    className={this.props.classes.ratingBarTop}
+                    style={{ height: (HEIGHT * (max - rating)) / max }}
+                />
+            </div>
+        ));
+        return (
+            <div
+                className={this.props.classes.ratingsChart}
+                style={{ height: HEIGHT }}
+            >
+                {elements}
+                <h1 className={classes.ratingsTitle}>Ratings</h1>
+                <div className={classes.avgRatingContainer}>
+                    <Typography className={classes.avgRating}>
+                        {avgRating}
+                    </Typography>
+                    <Typography className={classes.ratingsCount}>
+                        {ratingCount} ratings
+                    </Typography>
+                </div>
+            </div>
+        );
+    };
+
     render() {
         const { classes } = this.props;
         const { album } = this.state;
+
+        const ratings = [2, 3, 5, 4, 7, 22, 33, 94, 36, 54, 30];
+        const ratingElements = this.createBarChartElements(ratings);
+        console.log(ratingElements);
 
         if (album) {
             console.log(album);
@@ -137,7 +251,22 @@ export class album extends Component {
                             playcount: {album.playcount}
                         </Typography>
                     </Paper>
-                    <div className={classes.tracks}>{songs}</div>
+                    {ratingElements}
+                    <Typography
+                        color="primary"
+                        className={classes.tracklistExpansion}
+                        onClick={this.handleTrackListExpansion}
+                    >
+                        Tracklist
+                    </Typography>
+                    <div
+                        className={classes.tracks}
+                        style={{
+                            height: this.state.trackListOpened ? 'auto' : 0
+                        }}
+                    >
+                        {songs}
+                    </div>
                 </Grid>
             );
         } else {
