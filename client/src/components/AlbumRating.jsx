f3ddfb3a0b0d38f503bab96ba7be46276cbb5341
Jared Cox <coxj1@mcmaster.ca>
2020-08-11 11:34:53 -0400

    made ratings a separate element

---
+++ client/src/components/AlbumRating.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+
+//MUI
+import withStyles from '@material-ui/core/styles/withStyles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = (theme) => ({
+    ...theme.styleSpreading,
+    ratingsChart: {
+        width: '100%',
+        position: 'relative',
+        backgroundColor: '#212122',
+        display: 'flex',
+        padding: 15,
+        marginTop: 20
+    },
+    ratingBar: {
+        width: 50,
+        backgroundColor: '#e9e950',
+        margin: '0 3px'
+    },
+    ratingBarTop: {
+        width: 50,
+        backgroundColor: '#212122'
+    },
+    ratingsTitle: {
+        color: '#e9e950',
+        fontSize: 15,
+        fontWeight: 400,
+        position: 'absolute',
+        top: 10,
+        left: 15
+    },
+    avgRatingContainer: {
+        width: 200,
+        height: 200,
+        margin: 20
+    },
+    avgRating: {
+        color: '#fff',
+        fontSize: 45,
+        fontWeight: 500,
+        width: '100%',
+        textAlign: 'center'
+    },
+    ratingsCount: {
+        color: '#aaa',
+        fontSize: 15,
+        width: '100%',
+        textAlign: 'center'
+    }
+});
+
+export class AlbumRating extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    createBarChartElements = (ratings) => {
+        const { classes } = this.props;
+
+        const ratingCount = ratings.reduce(
+            (acc, ratings) => (acc += ratings),
+            0
+        );
+        const avgRating =
+            Math.round(
+                (ratings.reduce((acc, rating, i) => (acc += rating * i), 0) /
+                    ratingCount) *
+                    10
+            ) / 10;
+
+        let max = Math.max(...ratings);
+        let HEIGHT = 140;
+
+        let elements = ratings.map((rating) => (
+            <div
+                className={this.props.classes.ratingBar}
+                style={{ height: HEIGHT }}
+            >
+                <div
+                    className={this.props.classes.ratingBarTop}
+                    //Added plus one so the bars never have 0 height which would look weird
+                    style={{ height: (HEIGHT * (max - rating)) / max + 1 }}
+                />
+            </div>
+        ));
+        return (
+            <div
+                className={this.props.classes.ratingsChart}
+                style={{ height: HEIGHT }}
+            >
+                {elements}
+                <h1 className={classes.ratingsTitle}>Ratings</h1>
+                <div className={classes.avgRatingContainer}>
+                    <Typography className={classes.avgRating}>
+                        {avgRating}
+                    </Typography>
+                    <Typography className={classes.ratingsCount}>
+                        {ratingCount} ratings
+                    </Typography>
+                </div>
+            </div>
+        );
+    };
+
+    render() {
+        const { ratings } = this.props;
+
+        const albumRatingChart = this.createBarChartElements(ratings);
+
+        return <div>{albumRatingChart}</div>;
+    }
+}
+
+export default withStyles(styles)(AlbumRating);
--- client/src/pages/album.jsx
+++ client/src/pages/album.jsx
@@ -5,6 +5,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
+import AlbumRating from '../components/AlbumRating';
 
 const styles = (theme) => ({
     ...theme.styleSpreading,
@@ -78,49 +79,7 @@ const styles = (theme) => ({
         fontSize: 20,
         color: '#aaa'
     },
-    ratingsChart: {
-        width: '100%',
-        position: 'relative',
-        backgroundColor: '#212122',
-        display: 'flex',
-        padding: 15,
-        marginTop: 20
-    },
-    ratingBar: {
-        width: 50,
-        backgroundColor: '#e9e950',
-        margin: '0 3px'
-    },
-    ratingBarTop: {
-        width: 50,
-        backgroundColor: '#212122'
-    },
-    ratingsTitle: {
-        color: '#e9e950',
-        fontSize: 15,
-        fontWeight: 400,
-        position: 'absolute',
-        top: 10,
-        left: 15
-    },
-    avgRatingContainer: {
-        width: 200,
-        height: 200,
-        margin: 20
-    },
-    avgRating: {
-        color: '#fff',
-        fontSize: 45,
-        fontWeight: 500,
-        width: '100%',
-        textAlign: 'center'
-    },
-    ratingsCount: {
-        color: '#aaa',
-        fontSize: 15,
-        width: '100%',
-        textAlign: 'center'
-    }
+
 });
 
 const _api_key = process.env.REACT_APP_LASTFM_API_KEY;
@@ -149,67 +108,11 @@ export class album extends Component {
         this.setState({ trackListOpened: !this.state.trackListOpened });
     };
 
-    createBarChartElements = (ratings) => {
-        const { classes } = this.props;
-
-        const ratingCount = ratings.reduce(
-            (acc, ratings) => (acc += ratings),
-            0
-        );
-        const avgRating =
-            Math.round(
-                (ratings.reduce((acc, rating, i) => (acc += rating * i), 0) /
-                    ratingCount) *
-                    10
-            ) / 10;
-
-        let max = Math.max(...ratings);
-        let HEIGHT = 140;
-        console.log(max);
-        console.log((HEIGHT / max) * (max - ratings[0]));
-        console.log((HEIGHT / max) * (max - ratings[1]));
-        console.log((HEIGHT / max) * (max - ratings[2]));
-        console.log((HEIGHT / max) * (max - ratings[3]));
-        console.log((HEIGHT / max) * (max - ratings[4]));
-        console.log((HEIGHT / max) * (max - ratings[5]));
-
-        let elements = ratings.map((rating) => (
-            <div
-                className={this.props.classes.ratingBar}
-                style={{ height: HEIGHT }}
-            >
-                <div
-                    className={this.props.classes.ratingBarTop}
-                    style={{ height: (HEIGHT * (max - rating)) / max }}
-                />
-            </div>
-        ));
-        return (
-            <div
-                className={this.props.classes.ratingsChart}
-                style={{ height: HEIGHT }}
-            >
-                {elements}
-                <h1 className={classes.ratingsTitle}>Ratings</h1>
-                <div className={classes.avgRatingContainer}>
-                    <Typography className={classes.avgRating}>
-                        {avgRating}
-                    </Typography>
-                    <Typography className={classes.ratingsCount}>
-                        {ratingCount} ratings
-                    </Typography>
-                </div>
-            </div>
-        );
-    };
-
     render() {
         const { classes } = this.props;
         const { album } = this.state;
 
         const ratings = [2, 3, 5, 4, 7, 22, 33, 94, 36, 54, 30];
-        const ratingElements = this.createBarChartElements(ratings);
-        console.log(ratingElements);
 
         if (album) {
             console.log(album);
@@ -251,7 +154,7 @@ export class album extends Component {
                             playcount: {album.playcount}
                         </Typography>
                     </Paper>
-                    {ratingElements}
+                    <AlbumRating ratings={ratings} />
                     <Typography
                         color="primary"
                         className={classes.tracklistExpansion}
