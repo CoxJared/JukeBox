c41788f158486458fadd3345c62e475a2a416a59
Owen <17omc3@queensu.ca>
2020-08-16 19:30:29 -0400

    Added multiple reviews to the activity page
    created a function to add a review to the page using the writeReview function.

--- client/src/pages/activity.jsx
+++ client/src/pages/activity.jsx
@@ -6,6 +6,8 @@ import Avatar from '@material-ui/core/Avatar';
 import Paper from '@material-ui/core/Paper';
 
 import titanicRising from '../images/tempAlbumCovers/titanicrising-weyesblood.jpg';
+import closer from '../images/tempAlbumCovers/closer.jpg';
+import inrainbows from '../images/tempAlbumCovers/inrainbows.jpeg';
 import Star from '../images/star.png';
 
 
@@ -81,8 +83,9 @@ const styles = (theme) => ({
     albumInfo: {
       backgroundColor: '#212122',
       padding: '5px 15px',
-      margin: '10px',
-      borderRadius: 10
+      margin: '15px',
+      borderRadius: 10,
+      width: '1500px'
     },
 
     starRating:{
@@ -110,54 +113,114 @@ const styles = (theme) => ({
     Star:{
       height: '10px',
       width:'10px,'
-    }
+    },
 
+    Rating:{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'left',
+      alignItems: 'center',
+      margin:'5px',
+    }
 });
 
+function displayStars({rating,classes}){
+  if (rating == 1){
+      return <div className = {classes.Rating}>
+        <img className = {classes.Star} src= {Star} alt = ''/>
+        </div>
+      ;
+  }
+  else if (rating == 2){
+      return <div className = {classes.Rating}>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      </div>
+      ;
+  }
+  else if (rating == 3){
+      return <div className = {classes.Rating}>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      </div>
+      ;
+  }
+  else if (rating == 4){
+      return <div className = {classes.Rating}>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      </div>
+      ;
+  }
+  else if (rating == 5){
+      return  <div className = {classes.Rating}>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>
+      <img className = {classes.Star} src= {Star} alt = ''/>;
+      </div>
+  }
+  else{
+    return <div className = {classes.Rating}> No rating</div>
+  }
+  }
 
 
 
+function WriteReview  ({title, year, albumCover, review, faves ,user, rate,  classes}) {
+  // const {classes} = this.props;
+  return(
+    <Paper elevation={3} className = {classes.albumInfo}>
+        <div className={classes.container}>
+          <img className={classes.albumImage} src={albumCover} alt= {title}/>
+
+          <div>
+            <h2 className = {classes.userName}> 
+              <Avatar src="/no-image.png" className = {classes.avatar} /> 
+              <span> {user} </span> 
+            </h2>
+
+            {displayStars({rating:rate ,classes})}
+
+            <h1 className = {classes.textBox}>
+            <h1 className={classes.title}> {title} </h1>
+            <h1 className={classes.year}> {year} </h1> 
+            </h1>
+            <h2 className = {classes.text}>
+              {review}
+            </h2>
+            <h2 className = {classes.textBox}>
+            <h2 className= {classes.text}> Favourite Tracks:</h2>
+            <h2 className = {classes.faves}> {faves} </h2>
+            </h2>
+
+          </div>
+        </div>
+    </Paper>
+  )
+}
 
 export class activity extends Component {
 
   render() {
     const {classes} = this.props;
     return(
-      <Grid container
-      direction="row"
-      justify="flex-start"
-      alignItems="flex-start"> 
-      <Paper elevation={3} className = {classes.albumInfo}>
-        <div className={classes.container}>
-          <img className={classes.albumImage} src={titanicRising} alt="Titanic Rising Album Cover"/>
+    <Grid container
+    direction="row"
+    justify="flex-start"
+    alignItems="flex-start"> 
 
-          <div>
-            <h2 className = {classes.userName}> 
-              <Avatar src="/no-image.png" className = {classes.avatar} /> 
-              <span> Owen Cox </span> 
-            </h2>
-
-            <img className = {classes.Star} src= {Star} alt="Star"/>
-
-            <h1 className = {classes.textBox}>
-            <h1 className={classes.title}>Titanic Rising </h1>
-            <h1 className={classes.year}> 2019</h1> 
-            </h1>
-            <h2 className = {classes.text}>
-              Great album lotta good songs super pleasent on the ears blahblahblahblah love the album artwork very nice and cool and wet.
-            </h2>
-            <h2 className = {classes.textBox}>
-            <h2 className= {classes.text}> Favourite Tracks:</h2>
-            <h2 className = {classes.faves}> Movies, Alot's Gonna Change, Evertime </h2>
-            </h2>
-
-          </div>
-        </div>
-      </Paper>
-      </Grid>
+        < WriteReview title = 'Titanic Rising' year = '2019' albumCover = {titanicRising} review = 'Good Stuff' faves = 'Movies' user = 'Owen Cox' rate = '5' classes = {classes}/>
+        < WriteReview title = 'In Rainbows' year = '2007' albumCover = {inrainbows} review = 'Overrated garbage!!' faves = 'Nothing' user = 'Mochi' rate = '4' classes = {classes}/>
+        < WriteReview title = 'Closer' year = '1980' albumCover = {closer} review = 'Such a Classic!' faves = 'Closer idk didnt listen' user = 'Jared' rate = '1' classes = {classes}/>
+        {/* < displayStars rating = '5' classes = {classes}/> */}
+    </Grid>
     );
   }
 
 }
-
 export default withStyles(styles)(activity);
